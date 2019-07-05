d79dca58ac28804d51324ed4ec5601049b69eef6
Sebastian Wålinder <s.walinder@gmail.com>
2019-07-06 00:41:59 +0200

    Formatting, fixed firefox

--- configuration.nix
+++ configuration.nix
@@ -1,3 +1,6 @@
+# This install an arbritary ghc version
+# nix-shell -p haskell.compiler.ghcXYZ
+
 { config, pkgs, ... }:
 
 let
@@ -8,11 +11,11 @@ in
   system.stateVersion = "19.03";
 
   imports = [
-      # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
 
-      # ./build.nix
-    ];
+    # ./build.nix
+  ];
 
   hardware = {
     # enableAllFirmware = true;
@@ -26,22 +29,24 @@ in
   # hardware.pulseaudio.enable = true;
 
   nixpkgs.config = {
-      packageOverrides = pkgs: {
-      emacs = (pkgs.emacs.override { srcRepo = true; imagemagick = false; }).overrideAttrs (old: rec {
-         name = "emacs-${version}${versionModifier}";
-              version = "27.0.50";
-              versionModifier = "-git";
-              src = pkgs.fetchFromGitHub {
-                owner = "emacs-mirror";
-                repo = "emacs";
-
-                rev = "e751431989f0af69a7eabf26a86ccac8d8aab74f";
-                sha256 = "0alb8g9bflkk5yy5wrk069ihbwynyib2wilz36125ibs7p6l084h";
-              };
-             patches = [];
-            });
-
+    packageOverrides = pkgs: {
+      emacs = (pkgs.haskellPackages.halive.override { srcRepo = true; imagemagick = false; }).overrideAttrs (old: rec {
     
+      emacs = (pkgs.emacs.override { srcRepo = true; imagemagick = false; }).overrideAttrs (old: rec {
+        name = "emacs-${version}${versionModifier}";
+        version = "27.0.50";
+        versionModifier = "-git";
+        src = pkgs.fetchFromGitHub {
+          owner = "emacs-mirror";
+          repo = "emacs";
+
+          rev = "e751431989f0af69a7eabf26a86ccac8d8aab74f";
+          sha256 = "0alb8g9bflkk5yy5wrk069ihbwynyib2wilz36125ibs7p6l084h";
+        };
+        patches = [];
+      });
+
+
       unstable = import unstableTarball {
         config = config.nixpkgs.config;
       };
@@ -53,49 +58,54 @@ in
   nix.binaryCaches = [ "https://cache.nixos.org/" "https://nixcache.reflex-frp.org" ];
   nix.binaryCachePublicKeys = [ "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI=" ];
 
+  nixpkgs.config.firefox.enablePlasmaBrowserIntegration = true;
+
   environment.systemPackages = with pkgs; [
-     # xorg.xauth 
-     # xorg.xinit
-     xorg.setxkbmap
-     xorg.xrandr
-     
-     unstable.mono unstable.guile unstable.gcc
+    # xorg.xauth 
+    # xorg.xinit
+    xorg.setxkbmap
+    xorg.xrandr
 
-     mlocate atool unzip ag unstable.imagemagick wget openssh ffmpeg p7zip
+    unstable.mono unstable.guile unstable.gcc
 
-     aspell aspellDicts.en aspellDicts.sv
+    mlocate atool unzip ag unstable.imagemagick wget openssh ffmpeg p7zip
 
-     mpd mpc_cli
-     htop
-     poppler
 
-     # Haskell
-     ghc cabal-install cabal2nix 
+    aspell aspellDicts.en aspellDicts.sv
 
-     # Haskell editor
-     hlint haskellPackages.hoogle
-     
-     #binutils
-     
-     # Install stable HIE for GHC845 (GHC 864 and 863 is commented out). Looks like the binaries only exist for stable versions of ghc (ghc864)
-      (all-hies.selection { selector = p: { inherit (p) /* ghc864 ghc863 */ ghc864; }; })
- 
-     # Applications
-     emacs 
-     unstable.firefox unstable.git unstable.git-lfs unstable.redshift unstable.gimp mpv
-     pavucontrol
-     
-     harfbuzz
+    mpd mpc_cli
+    htop
+    poppler
 
-     dovecot
-     
-     direnv
+    # Haskell
+    ghc haskellPackages.halive cabal-install cabal2nix 
 
-     # Security
-     gnupg unstable.pinentry_emacs
+    # Haskell editor
+    hlint haskellPackages.hoogle
 
-     # Virtualbox
-     linuxPackages.virtualboxGuestAdditions
+
+    #binutils
+
+    # Install stable HIE for GHC845 (GHC 864 and 863 is commented out). Looks like the binaries only exist for stable versions of ghc (ghc864)
+    (all-hies.selection { selector = p: { inherit (p) /* ghc864 ghc863 */ ghc864; }; })
+
+    # Applications
+    emacs 
+    plasma-browser-integration
+    firefox unstable.git unstable.git-lfs unstable.redshift unstable.gimp mpv
+    pavucontrol
+
+    harfbuzz
+
+    dovecot
+
+    direnv
+
+    # Security
+    gnupg unstable.pinentry_emacs
+
+    # Virtualbox
+    linuxPackages.virtualboxGuestAdditions
   ];
 
   fonts.fonts = [ pkgs.inconsolata-lgc pkgs.dejavu_fonts ];
@@ -108,7 +118,7 @@ in
   # boot.loader.grub.efiSupport = true;
   # boot.loader.grub.efiInstallAsRemovable = true;
   # boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  
+
   # Enable BIOS support
   boot.loader.grub.device = "/dev/sda";
 
@@ -117,11 +127,9 @@ in
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Select internationalisation properties.
-  # i18n = {
-  #   consoleFont = "Lat2-Terminus16";
-  #   consoleKeyMap = "carpalx";
-  #   defaultLocale = "en_US.UTF-8";
-  # };
+  #  i18n.consoleFont = "Lat2-Terminus16";
+  #  i18n.consoleKeyMap = "carpalx";
+  #  i18n.defaultLocale = "en_US.UTF-8";
 
   # Set your time zone.
   time.timeZone = "Europe/Amsterdam";
@@ -144,8 +152,8 @@ in
   # services.printing.enable = true;
 
   # Enable sound.
-   sound.enable = true;
-   hardware.pulseaudio.enable = true;
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
 
   # Enable the X11 windowing system.
   # services.xserver.enable = true;
@@ -153,17 +161,20 @@ in
   # services.xserver.xkbOptions = "eurosign:e";
   #  services.xserver.displayManager.lightdm.enable = false;
 
-services = {
-  emacs.defaultEditor = true;
+# services.dovecot2
 
-  # SMART.
-  smartd = {
-    enable = true;
-    devices = [{ device = "/dev/sda"; }];
-    notifications.test = true;
-    notifications.x11.enable = true;
-  };
-  fstrim.enable = true;
+
+  services = {
+    emacs.defaultEditor = true;
+
+    # SMART.
+    smartd = {
+      enable = true;
+      devices = [{ device = "/dev/sda"; }];
+      notifications.test = true;
+      notifications.x11.enable = true;
+    };
+    fstrim.enable = true;
 
     physlock = {
       enable = true;
@@ -176,47 +187,47 @@ services = {
       };
     };
 
-# Enable the OpenSSH daemon.
-openssh = {
-  enable = true;
-  forwardX11 = true;
-};
+    # Enable the OpenSSH daemon.
+    openssh = {
+      enable = true;
+      forwardX11 = true;
+    };
 
-xserver = {
+    xserver = {
       enable = true;
       #autorun = false;
       layout = "us";
       #displayManager.lightdm.enable = true;
       #desktopManager.default = "none";
-  
+
       # Allow exwm to work
       displayManager.sessionCommands = "${pkgs.xorg.xhost}/bin/xhost +SI:localuser:$USER";
-  
+
       #  Set emacs as default entry
       desktopManager.default = "emacs";
       desktopManager.session = [ {
-      manage = "desktop";
-      name = "emacs";
-      start = ''
+        manage = "desktop";
+        name = "emacs";
+        start = ''
           #${pkgs.emacs}/bin/emacs &
           emacs &
           waitPID=$!
-      '';
+        '';
       }];
-  };
-  
-  # Enable the locate service, it doesn't work otherwise
-  locate = {
+    };
+
+    # Enable the locate service, it doesn't work otherwise
+    locate = {
       enable = true;
       # just never auto update the database, they use cron as a to run locate at time intervals, this expression makes it never evauate (unless the year is 3099)
       interval = "0 0 0 1 1 ? 3099";
+    };
+
+    # Define a user account. Don't forget to set a password with ‘passwd’.
   };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-};
-
   users = {
-      extraUsers.admin = {
+    extraUsers.admin = {
       group = "users";
 
       isNormalUser = true;
@@ -225,7 +236,7 @@ xserver = {
       extraGroups = [ "wheel" "audio" "video" "usbmux" "networkmanager" ];
       #"mlocate" 
       password = "";   
-      };
-      mutableUsers = true;
+    };
+    mutableUsers = true;
   };
 }
