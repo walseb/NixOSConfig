6aa86e411599c932cf95e821b8ec82fbfaed3c65
Sebastian Wålinder <s.walinder@gmail.com>
2020-07-07 10:06:04 +0200

    Updated laptop settings

--- hw-configs/generic/laptop.nix
+++ hw-configs/generic/laptop.nix
@@ -10,4 +10,8 @@
 
   # Hibernate after 1 hour of sleep
   systemd.sleep.extraConfig = "HibernateDelaySec=1h";
+
+  # Only turn off screen on lid close
+  services.logind.lidSwitch = "ignore";
+  services.logind.lidSwitchDocked = "ignore";
 }
