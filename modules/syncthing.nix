792dffdf69bbbaebe851aa018cd27bfd74299003
Sebastian Wålinder <s.walinder@gmail.com>
2020-07-17 18:46:41 +0200

    Removed syncthing data dir setting

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 {
   services.syncthing.enable = true;
-  services.syncthing.dataDir = "/home/admin/Sync";
+  # services.syncthing.dataDir = "/home/admin/Sync";
 
   services.syncthing.user = "admin";
   services.syncthing.group = "users";
