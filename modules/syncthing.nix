79000b4395489010fcfa3a0206fe5141820efa6d
Sebastian Wålinder <s.walinder@gmail.com>
2020-07-17 19:25:47 +0200

    Added back syncthing data dir

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 {
   services.syncthing.enable = true;
-  # services.syncthing.dataDir = "/home/admin/Sync";
+  services.syncthing.dataDir = "/home/admin/.cache/syncthing";
 
   services.syncthing.user = "admin";
   services.syncthing.group = "users";
