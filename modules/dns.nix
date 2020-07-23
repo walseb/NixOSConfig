9524b5d78aac4cab8297f96f83d6ade6d9414d10
Sebastian Wålinder <s.walinder@gmail.com>
2020-07-23 22:56:28 +0200

    Now using DNS relays

--- modules/dns.nix
+++ modules/dns.nix
@@ -1,6 +1,6 @@
-# https://nixos.wiki/wiki/Encrypted_DNS
 { config, pkgs, ... }:
 {
+  # https://nixos.wiki/wiki/Encrypted_DNS
   networking = {
     nameservers = [ "::1" ];
     # resolvconf.enable = false;
@@ -15,7 +15,8 @@
   services.dnscrypt-proxy2 = {
     enable = true;
     settings = {
-      # https://github.com/DNSCrypt/dnscrypt-proxy/blob/master/dnscrypt-proxy/example-dnscrypt-proxy.toml
+      # Sample config
+      # https://raw.githubusercontent.com/DNSCrypt/dnscrypt-proxy/master/dnscrypt-proxy/example-dnscrypt-proxy.toml
 
       force_tcp = false;
       ipv6_servers = false;
@@ -30,16 +31,36 @@
       sources.public-resolvers = {
         # https://github.com/DNSCrypt/dnscrypt-proxy/wiki/DNS-server-sources
         urls = [
-          "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v2/opennic.md"
-          "https://download.dnscrypt.info/resolvers-list/v2/opennic.md"
+          "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
+          "https://download.dnscrypt.info/dnscrypt-resolvers/v3/public-resolvers.md"
         ];
 
-        cache_file = "/var/lib/dnscrypt-proxy2/opennic.md";
+        cache_file = "/var/lib/dnscrypt-proxy2/public-resolvers.md";
         minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
       };
 
-      # You can choose a specific set of servers from https://github.com/DNSCrypt/dnscrypt-resolvers/blob/master/v2/public-resolvers.md
-      # server_names = [ ... ];
+      sources.relays = {
+        urls = [
+          "https://github.com/DNSCrypt/dnscrypt-resolvers/blob/master/v3/relays.md"
+          "https://download.dnscrypt.info/dnscrypt-resolvers/v3/relays.md"
+        ];
+
+        cache_file = "/var/lib/dnscrypt-proxy2/relays.md";
+        minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+        # refresh-delay = 100;
+      };
+
+      server_names = (import ../nixos-private-config/config.nix).myDnsServers;
+
+      anonymized_dns = {
+        # TODO: Requires ~2.0.42
+        # skip_incompatible = true;
+        # direct_cert_fallback = false;
+
+        routes = [
+          { server_name = "*"; via=(import ../nixos-private-config/config.nix).myDnsRelays; }
+        ];
+      };
     };
   };
 
