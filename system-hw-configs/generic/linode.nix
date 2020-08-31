80fe2cd2b590cdc908b9613b8e49dc1db29d379e
Sebastian Wålinder <s.walinder@gmail.com>
2020-08-31 21:21:51 +0200

    Added linode config

---
+++ system-hw-configs/generic/linode.nix
@@ -0,0 +1,15 @@
+{ lib, config, pkgs, ... }:
+with lib;
+{
+  boot.kernelParams = [ "console=ttyS0,19200n8" ];
+  boot.loader.grub.extraConfig = ''
+    serial --speed=19200 --unit=0 --word=8 --parity=no --stop=1;
+    terminal_input serial;
+    terminal_output serial;
+'';
+
+  boot.loader.grub.forceInstall = true;
+
+  networking.usePredictableInterfaceNames = false;
+  networking.interfaces.eth0.useDHCP = true;
+}
--- system-hw-configs/vps.nix
+++ system-hw-configs/vps.nix
@@ -4,5 +4,11 @@
   imports = [
     # ../system-modules/VPS/nextcloud.nix
     ../system-modules/boot/bios.nix
+    ./generic/linode.nix
   ];
+
+  networking.firewall.allowedTCPPorts = [ 22 ];
+  networking.firewall.allowedUDPPorts = [ 22 ];
+
+  services.sshd.enable = true;
 }
