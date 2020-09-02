745854bc0b754e6c958607dfbfd4445fa38cbef2
Sebastian Wålinder <s.walinder@gmail.com>
2020-09-02 04:49:19 +0200

    Nextcloud now might use https

--- system-modules/VPS/nextcloud.nix
+++ system-modules/VPS/nextcloud.nix
@@ -4,12 +4,17 @@
     hostName = "127.0.0.1";
     autoUpdateApps.enable = true;
 
+    https = true;
+    maxUploadSize = "100G";
+
     config = {
       dbuser = "nextcloud";
       dbhost = "/run/postgresql"; # nextcloud will add /.s.PGSQL.5432 by itself
       dbname = "nextcloud";
       adminuser = "admin";
       adminpassFile = "/home/admin/nextcloud-password";
+
+      overwriteProtocol = "https";
     };
   };
 
