67b619b3c7c65d3145b86a334cb80a04e401cc2d
Sebastian Wålinder <s.walinder@gmail.com>
2020-09-22 00:54:28 +0200

    Disabled features from template device config

--- home-device-template.nix
+++ home-device-template.nix
@@ -1,38 +1,45 @@
+# * Config
 { pkgs, ... }:
 {
   imports = [
-    ./home-modules/email.nix
+    # ./home-modules/email.nix
     # ./home-modules/games/cataclysm-dda/cataclysm-dda-git-latest.nix
     # ./home-modules/build-emacs.nix
     # ./home-modules/art.nix
     # ./home-modules/spotify.nix
     # ./home-modules/spotifyd.nix
   ];
+
+  # home.packages = with pkgs; [
+
+  # ];
+
+  # ** Git
   # programs.git = {
   #   signing = {
   #     key = "";
   #     signByDefault = true;
   #   };
   # };
-  home.packages = with pkgs; [
-  ];
 
-  accounts.email = {
-    accounts.main-gmail = {
-      passwordCommand = "echo gmail-app-password";
-    };
-  };
+  # ** Email
+  # accounts.email = {
+  #   accounts.main-gmail = {
+  #     passwordCommand = "echo gmail-app-password";
+  #   };
+  # };
 
-  services.spotifyd.settings =
-    {
-      global = {
-        username = "";
-        # The actual account password, not an api key or anything
-        password = "";
-        # Can't contain spaces
-        device_name = "";
+  # ** Spotifyd
+  # services.spotifyd.settings =
+  #   {
+  #     global = {
+  #       username = "";
+  #       # The actual account password, not an api key or anything
+  #       password = "";
+  #       # Can't contain spaces
+  #       device_name = "";
 
-        backend = "pulseaudio";
-      };
-    };
+  #       backend = "pulseaudio";
+  #     };
+  #   };
 }
