693fb2e3f042fe02abe66cda04a965b564856258
Sebastian Wålinder <s.walinder@gmail.com>
2020-10-03 22:02:14 +0200

    VPS network now works, etc

--- system-hw-configs/vps.nix
+++ system-hw-configs/vps.nix
@@ -3,16 +3,20 @@
 # Installed using nixos-in-place
 {
   imports = [
-    ../system-modules/boot/bios.nix { grub-dev = "/dev/vda"; }
+    (import ../system-modules/boot/bios.nix { grub-dev = "/dev/vda"; })
 
     ../system-modules/VPS/nextcloud.nix
     ../system-modules/auto-upgrade.nix
   ];
 
   networking.usePredictableInterfaceNames = false;
-  networking.interfaces.eth0.useDHCP = true;
 
-  networking.nameservers = [ "151.236.6.6 " "151.236.7.77" "1.1.1.1" ];
+  networking.interfaces.eth0.useDHCP = false;
+  networking.interfaces.eth0.ipv4.addresses = [ { address = "46.246.93.80"; prefixLength = 24;} ];
+  networking.defaultGateway = {
+    address = "46.246.93.1";
+    interface = "eth0";
+  };
 
   boot.kernelParams = ["boot.shell_on_fail"];
   boot.loader.grub.storePath = "/nixos/nix/store";
