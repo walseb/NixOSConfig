adda0509ead660c9256f92d55e5cda4ec246a115
Sebastian Wålinder <s.walinder@gmail.com>
2020-11-03 23:25:47 +0100

    Added flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+# https://github.com/terlar/nix-config
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }: {
+    nixosConfigurations = {
+      hostname = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.user = import ./home.nix;
+          }
+        ];
+      };
+    };
+  };
+}
