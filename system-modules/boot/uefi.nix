6ea1636d5594931955270eee724a6f7edb2ee770
Sebastian Wålinder <s.walinder@gmail.com>
2021-02-19 11:29:13 +0100

    Fixed efi support

--- system-modules/boot/uefi.nix
+++ system-modules/boot/uefi.nix
@@ -1,12 +1,12 @@
 {
-  imports = [
-    ./grub.nix
-  ];
+  #  imports = [
+  #    ./grub.nix
+  #  ];
 
   # Enable EFI support
-  boot.loader.grub.efiSupport = true;
-  boot.loader.grub.efiInstallAsRemovable = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
-
+  #  boot.loader.grub.efiSupport = true;
+  #  boot.loader.grub.efiInstallAsRemovable = true;
+  #  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.enable = true;
 }
