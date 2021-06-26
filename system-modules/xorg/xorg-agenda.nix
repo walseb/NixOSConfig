ffff9265ac29c110037281be3662e9f925b19e10
Sebastian Wålinder <s.walinder@gmail.com>
2021-06-26 20:23:46 +0200

    Attempt at making surf autoplay

--- system-modules/xorg/xorg-agenda.nix
+++ system-modules/xorg/xorg-agenda.nix
@@ -1,48 +1,51 @@
-{ pkgs, xrandr-output, file, xrandr-extra ? "", ... }:
-{
+{ pkgs, xrandr-output, file, xrandr-extra ? "", ... }: {
 
   environment.systemPackages = with pkgs; [
     xorg.xrandr
     inotify-tools
     surf
+    # (pkgs.surf.override { patches = ../../loose-configs/surf/autoplay.patch; })
+
     xterm
     dwm
+
+    # These don't expose their paths
+    # gst_all_1.gst-plugins-bad
+    # gst_all_1.gst-plugins-base
+    # gst_all_1.gst-plugins-good
   ];
 
   services.xserver = {
     enable = true;
     layout = "us";
 
-    displayManager.session =
-      [ { manage = "desktop";
-          name = "xterm";
-          start = ''
-            ${pkgs.xterm}/bin/xterm -ls &
-            waitPID=$!
-          '';
-        }
-      ];
+    displayManager.session = [{
+      manage = "desktop";
+      name = "xterm";
+      start = ''
+        ${pkgs.xterm}/bin/xterm -ls &
+        waitPID=$!
+      '';
+    }];
 
-    desktopManager.session = [
-      {
-        manage = "desktop";
-        name = "agenda";
-        start = ''
-            xrandr --output ${xrandr-output} ${xrandr-extra}
-            while true; do
-                ${pkgs.dwm}/bin/dwm &
-                PID=$!
-                ${pkgs.surf}/bin/surf ${file} &
-                PIDSURF=$!
-                ${pkgs.inotify-tools}/bin/inotifywait -e modify ${file}
-                # Give transfer some time to complete
-                # ${pkgs.coreutils}/bin/sleep 5
-                kill $PID
-                kill $PIDSURF
-            done
-          '';
-      }
-    ];
+    desktopManager.session = [{
+      manage = "desktop";
+      name = "agenda";
+      start = ''
+        xrandr --output ${xrandr-output} ${xrandr-extra}
+        while true; do
+            ${pkgs.dwm}/bin/dwm &
+            PID=$!
+            ${pkgs.surf}/bin/surf ${file} &
+            PIDSURF=$!
+            ${pkgs.inotify-tools}/bin/inotifywait -e modify ${file}
+            # Give transfer some time to complete
+            # ${pkgs.coreutils}/bin/sleep 5
+            kill $PID
+            kill $PIDSURF
+        done
+      '';
+    }];
 
     #  Set emacs as default entry
     displayManager.defaultSession = "agenda";
